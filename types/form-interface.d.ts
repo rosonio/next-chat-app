51f17130a596f108d6084203ff7e088fff871423
RazvanD <razvan.daniciuc@ecenta.com>
2024-05-25 17:58:35 +0300

    Created form interface

---
+++ types/form-interface.d.ts
@@ -0,0 +1,31 @@
+import { ReactNode, HTMLInputTypeAttribute } from "react";
+import {
+  Path,
+  UseFormReturn,
+  FieldValues,
+  SubmitHandler,
+} from "react-hook-form";
+
+export interface NextChatAppForm<T extends FieldValues> {
+  /**
+   * The handler for the form submit event.
+   *
+   * @see https://react-hook-form.com/docs/useform/handlesubmit
+   */
+  formSubmitHandler: SubmitHandler<T>;
+  /**
+   * The return of the useForm hook from react-hook-form.
+   *
+   * @see https://react-hook-form.com/docs/useform
+   */
+  form: UseFormReturn<T>;
+}
+export interface FormElementProps<FormFieldValues extends FieldValues> {
+  description?: ReactNode;
+  disabled?: boolean;
+  form: UseFormReturn<FormFieldValues>;
+  label?: string;
+  name: Path<FormFieldValues>;
+  placeholder?: string;
+  type?: HTMLInputTypeAttribute;
+}
