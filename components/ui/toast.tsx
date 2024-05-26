7910753e8004f2cc00716fb201da05feb6bf3fe9
RazvanD <razvan.daniciuc@ecenta.com>
2024-05-26 09:02:50 +0300

    Added variant of success to toast

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -32,6 +32,7 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
+        success: "bg-emerald-500/15 border text-emerald-500",
       },
     },
     defaultVariants: {
