2de36509fd5727e657b5b1f9250651f1148e079c
RazvanD <razvan.daniciuc@ecenta.com>
2024-05-26 08:00:49 +0300

    Added toaster to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import { PropsWithChildren } from "react";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function RootLayout(props: PropsWithChildren) {
   return (
     <html lang="en">
       <body>{props.children}</body>
+      <Toaster />
     </html>
   );
 }
