0f8a2e56e42402e41fd7a877cdf0a19514a35312
RazvanD <razvan.daniciuc@ecenta.com>
2024-06-03 06:42:58 +0300

    If there is a session, redirect user to dashboard

--- app/(unauthenticated)/layout.tsx
+++ app/(unauthenticated)/layout.tsx
@@ -1,6 +1,16 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react";
 
 export default function UnauthenticatedLayout(props: PropsWithChildren) {
+  const session = useSession();
+
+  if (session.status === "authenticated") {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="h-full flex items-center justify-center">
       {props.children}
