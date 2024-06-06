b697c190bef57ec64b79b6f6ad2627b18e95026a
RazvanD <razvan.daniciuc@ecenta.com>
2024-06-07 00:55:47 +0300

    Messages hooks and action

---
+++ app/actions/getMessages.ts
@@ -0,0 +1,22 @@
+import prisma from "@/lib/prismadb";
+import ConversationId from "../(authenticated)/conversations/[conversationId]/page";
+
+const getMessages = async (conversationId: string) => {
+  try {
+    const messages = await prisma.message.findMany({
+      where: {
+        conversationId: conversationId,
+      },
+      include: { sender: true, seen: true },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    return messages;
+  } catch (error) {
+    return [];
+  }
+};
+
+export default getMessages;
