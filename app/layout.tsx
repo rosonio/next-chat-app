bc91c920a939216d0583cb8c785a5561c16abd1c
RazvanD <razvan.daniciuc@ecenta.com>
2024-06-06 22:51:14 +0300

    SessionContext to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import { PropsWithChildren } from "react";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import SessionContext from "@/utils/SessionContext";
 
 export default function RootLayout(props: PropsWithChildren) {
   return (
     <html lang="en">
       <body>
-        <Toaster />
-        {props.children}
+        <SessionContext>
+          <Toaster />
+          {props.children}
+        </SessionContext>
       </body>
     </html>
   );
