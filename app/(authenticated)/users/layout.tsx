8c0fb579dde95dca55e500bf2688a929d74ed1ad
RazvanD <razvan.daniciuc@ecenta.com>
2024-06-14 11:19:44 +0300

    Fixed users list for mobile view

--- app/(authenticated)/users/layout.tsx
+++ app/(authenticated)/users/layout.tsx
@@ -1,9 +1,13 @@
+import getUsers from "@/app/actions/getUsers";
 import Sidebar from "@/components/sidebar/sidebar";
+import UserList from "@/components/user-components/user-list";
 import { PropsWithChildren } from "react";
 
 export default async function AuthenticatedLayout(props: PropsWithChildren) {
+  const users = await getUsers();
   return (
     <Sidebar>
+      <UserList items={users} />
       <div className="h-full">{props.children}</div>
     </Sidebar>
   );
