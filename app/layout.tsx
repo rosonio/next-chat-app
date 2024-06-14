d4640c6cb6ccb6903679c406488964110ae4503f
RazvanD <razvan.daniciuc@ecenta.com>
2024-06-14 12:28:48 +0300

    Added speed insights for vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,14 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import SessionContext from "@/utils/SessionContext";
 import ActiveStatus from "@/components/active-status";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout(props: PropsWithChildren) {
   return (
     <html lang="en">
       <body>
         <SessionContext>
+          <SpeedInsights />
           <Toaster />
           <ActiveStatus />
           {props.children}
